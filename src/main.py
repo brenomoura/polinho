fe715424b73aca8e2f5a6b6a2abcf03e91aaa596
Breno Moura <breno.mm@outlook.com>
2024-02-09 00:05:17 -0300

    feat: add balance insconsistency exception for invalid requests

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from socketify import App
 
 from models.transaction import CreateTransaction
 from services.customer_service import get_customer_info
+from services.exceptions import BalanceInconsistency
 from services.transaction_service import process_transaction
 
 app = App()
@@ -18,7 +19,7 @@ logging.basicConfig(
 
 def log(handler):
     async def log_route(res, req):
-        logging.info(f"{req.get_method()}{req.get_full_url()} {req.get_headers()}")
+        logging.info(f"{req.get_method()} {req.get_full_url()} {req.get_headers()}")
         await handler(res, req)
 
     return log_route
@@ -43,12 +44,10 @@ async def create_transaction(res, req):
         transaction_creation_data = CreateTransaction.from_dict(transacation_data)
         balance_info = process_transaction(customer_id, transaction_creation_data)
         res.write_status(200).cork_end(balance_info.to_dict())
-    except (MissingField, InvalidFieldValue):
-        error_msg = "validation fail, check if any missing field or wrongs types"
+    except (MissingField, InvalidFieldValue, BalanceInconsistency):
+        error_msg = "validation fail, or inconsistency check if any missing field, wrongs types, or "
         logging.exception(error_msg)
         res.write_status(422).cork_end(error_msg)
-    except Exception:
-        res.write_status(500)
 
 
 @log
